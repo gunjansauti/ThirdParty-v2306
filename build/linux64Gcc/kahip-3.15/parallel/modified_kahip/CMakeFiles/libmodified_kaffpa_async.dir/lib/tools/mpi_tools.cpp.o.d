65609b9ccd8fdea8060db49979b1cc029b8d597f
gunjansauti <gunjansauti@gmail.com>
2024-07-13 17:19:03 +0900

    second commit

---
+++ build/linux64Gcc/kahip-3.15/parallel/modified_kahip/CMakeFiles/libmodified_kaffpa_async.dir/lib/tools/mpi_tools.cpp.o.d
@@ -0,0 +1,26 @@
+parallel/modified_kahip/CMakeFiles/libmodified_kaffpa_async.dir/lib/tools/mpi_tools.cpp.o: \
+ /home/auti/openfoam/ThirdParty-v2306/kahip-3.15/parallel/modified_kahip/lib/tools/mpi_tools.cpp \
+ /usr/include/stdc-predef.h \
+ /home/auti/.sharedfilesystem/.softwares/openmpi/include/mpi.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stddef.h \
+ /home/auti/.sharedfilesystem/.softwares/openmpi/include/mpi_portable_platform.h \
+ /usr/include/unistd.h /usr/include/features.h \
+ /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/posix_opt.h \
+ /usr/include/x86_64-linux-gnu/bits/environments.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/confname.h \
+ /usr/include/x86_64-linux-gnu/bits/getopt_posix.h \
+ /usr/include/x86_64-linux-gnu/bits/getopt_core.h \
+ /usr/include/x86_64-linux-gnu/bits/unistd.h \
+ /usr/include/x86_64-linux-gnu/bits/unistd_ext.h \
+ /usr/include/linux/close_range.h \
+ /home/auti/openfoam/ThirdParty-v2306/kahip-3.15/parallel/modified_kahip/lib/tools/mpi_tools.h
---
+++ build/linux64Gcc/kahip-3.15/parallel/parallel_src/CMakeFiles/toolbox.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/auti/openfoam/ThirdParty-v2306/kahip-3.15/parallel/parallel_src/app/toolbox.cpp" "parallel/parallel_src/CMakeFiles/toolbox.dir/app/toolbox.cpp.o" "gcc" "parallel/parallel_src/CMakeFiles/toolbox.dir/app/toolbox.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/auti/openfoam/ThirdParty-v2306/build/linux64Gcc/kahip-3.15/parallel/modified_kahip/CMakeFiles/libmodified_kahip_interface.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
